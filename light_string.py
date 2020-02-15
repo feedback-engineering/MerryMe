8941f8d5fe3154d2157240d8a0d5326f0eb555e7
Liam Stevens <me@liamstevens.me>
2020-02-15 23:24:13 +1000

    modified colours to be tuple rather than list. Added send functionality to string.

--- light_string.py
+++ light_string.py
@@ -1,16 +1,23 @@
 from rgb_led import led
-
+import neopixel, machine
 class lightString:
-    currentLights = []
+    lights = []
     
 
     def addLight(self, newLight):
-        currentLights.append(newLight)
+        self.lights.append(newLight)
     
     def updateLights(self, newColours):
-        #newColours is a list of 2-tuples, the first element of which is a list of RGB values, and the second value is the brightness scaling factor
-        if len(newColours) == len(currentLights):
+        #newColours is a list of 2-tuples, the first element of which is a 3-tuple of RGB values (in format R, G, B), and the second value is the brightness scaling factor
+        if len(newColours) == len(self.lights):
         
-            for e in newColours:
+            for i in range(0, len(newColours),1):
+                self.lights[i].updateColour(newColours[i][0], newColours[i][1])
+
         else:
             raise Exception("Number of lights to update does not match number of lights within string.")
+
+    def sendString(self):
+        np = neopixel.NeoPixel(machine.Pin(5), len(self.lights))
+        for i in range(0, len(self.lights),1):
+            np[i] = self.lights[i].getColour()
--- rgb_led.py
+++ rgb_led.py
@@ -1,19 +1,24 @@
+import math
 
 class led:
     state = False
-    colour = [0,0,0]
+    colour = (0,0,0)
     brightness = 0 
     id = None
 
-    def __init__(self, state=False, colour=[0,0,0], brightness=0, id=None):
+    def __init__(self, state=False, colour=(0,0,0), brightness=0, id=None):
         self.state = state
         self.colour = colour
         self.brightness = brightness
         self.id = id
+
         
 
     def __str__(self):
-        print "ID:"+self.id+", State:"+self.state+", Colour:"+self.colour+", Brightness:"+self.brightness
+        print("ID:"+self.id+", State:"+self.state+", Colour:"+self.colour+", Brightness:"+self.brightness)
 
-    def updateColour(self, r, g, b):
-        self.colour = [r,g,b]
+    def getColour(self):
+        return self.colour
+
+    def updateColour(self, rgb, brightness):
+        self.colour = (math.floor(rgb[0]*(brightness/255)),math.floor(rgb[1]*(brightness/255)),math.floor(rgb[2]*(brightness/255)))
